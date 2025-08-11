5d3447caa482970b78d40540c5996e9c9ee5281f
sysol8 <iabrosimov@list.ru>
2025-08-11 19:01:17 +0300

    Кастомный хук для работы с API: получение списка постов, удаление, редактирование и добавление поста + состояние загрузки и ошибки

---
+++ src/hooks/usePosts.ts
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import {
+  createPost as apiCreatePost,
+  deletePost as apiDeletePost,
+  getPosts as apiGetPosts,
+} from "../api/posts.ts";
+import type { IPost, ICreatePost } from "../utils/types.ts";
+
+interface UsePostsData {
+  posts: IPost[];
+  loading: boolean;
+  error: unknown;
+}
+
+interface UsePostsActions {
+  addPost: (content: ICreatePost) => Promise<void>;
+  deletePost: (id: string) => Promise<void>;
+}
+
+type TUsePosts = UsePostsData & UsePostsActions;
+
+function usePosts(): TUsePosts {
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<unknown | null>(null);
+
+  const addPost = async (content: ICreatePost) => {
+    try {
+      const newPost = await apiCreatePost(content);
+      setPosts((prev) => [newPost, ...prev]);
+    } catch (error: unknown) {
+      console.error(error);
+      setError(error);
+    }
+  };
+
+  const deletePost = async (id: string) => {
+    try {
+      await apiDeletePost(id);
+      setPosts((prev) => prev.filter((post) => post.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    let isMounted: boolean = true;
+    setError(null);
+
+    (async () => {
+      try {
+        const data: IPost[] = await apiGetPosts();
+        if (!isMounted) return;
+        setPosts(data);
+      } catch (error) {
+        if (isMounted) setError(error);
+      } finally {
+        if (isMounted) setLoading(false);
+      }
+    })();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+  return { posts, loading, error, addPost, deletePost };
+}
+
+export default usePosts;
