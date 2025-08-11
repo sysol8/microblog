7cb70d6bd830d9e250a056a4f953fee361941b43
sysol8 <iabrosimov@list.ru>
2025-08-11 18:50:48 +0300

    Вынес загрузки списка постов, добавления и удаления поста в кастомный хук

--- src/layouts/PostsLayout/PostsLayout.tsx
+++ src/layouts/PostsLayout/PostsLayout.tsx
@@ -1,52 +1,15 @@
 import styles from "./PostsLayout.module.css";
-import { useState, useEffect } from "react";
 import PostsList from "../../components/PostsList/PostsList.tsx";
 import PostForm from "../../components/PostForm/PostForm.tsx";
-import type { IPost } from "../../utils/types.ts";
-import { getPosts, createPost, deletePost } from "../../api/posts.ts";
+import usePosts from "../../hooks/usePosts.ts";
 
 export default function PostsLayout() {
-  const [posts, setPosts] = useState<IPost[]>([]);
-
-  useEffect(() => {
-    async function loadPosts() {
-      try {
-        const _posts = await getPosts();
-        setPosts(_posts);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    let active = true;
-    loadPosts();
-    return () => {
-      active = false;
-    };
-  }, []);
-
-  const handlePostAdd = async (content: string) => {
-    try {
-      const newPost = await createPost(content);
-      setPosts((prev) => [newPost, ...prev]);
-    } catch (error: unknown) {
-      console.error(error);
-    }
-  };
-
-  const handlePostDelete = async (id: number) => {
-    try {
-      await deletePost(id);
-      setPosts((prev) => prev.filter((post) => post.id !== id));
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const { posts, deletePost, addPost } = usePosts();
 
   return (
     <main className={styles.content}>
-      <PostsList posts={posts} onDelete={handlePostDelete}></PostsList>
-      <PostForm onPostAdd={handlePostAdd}></PostForm>
+      <PostsList posts={posts} onDelete={deletePost}></PostsList>
+      <PostForm onPostAdd={addPost}></PostForm>
     </main>
   );
 }
