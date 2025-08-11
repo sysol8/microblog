015badbbf0f4b25b7bb47644613bac2978a5fe59
sysol8 <iabrosimov@list.ru>
2025-08-11 18:48:48 +0300

    Изменил схемы в соответствии с новой моделью данных

--- server/app/posts/schemas.py
+++ server/app/posts/schemas.py
@@ -1,21 +1,17 @@
 from datetime import datetime
+from typing import List
 from pydantic import BaseModel, Field, ConfigDict
 
 class PostCreate(BaseModel):
-    content: str
+    text_content: str
 
 class PostUpdate(BaseModel):
-    content: str
+    text_content: str | None = None
 
 class PostRead(BaseModel):
-    model_config = ConfigDict(
-        from_attributes=True,
-        populate_by_name=True,
-    )
+    model_config = ConfigDict(from_attributes=True, populate_by_name=True)
 
     id: int
-    content: str
-    created_at: datetime = Field(alias='createdAt')
-
-pr = PostRead.model_validate({ 'id': 1, 'content': '123', 'createdAt': '2025-07-02T14:19:13.200266'})
-print(pr.model_dump(by_alias=True))
+    text_content: str = Field(alias="textContent")
+    image_urls: List[str] = Field(alias="imageUrls")
+    created_at: datetime = Field(alias="createdAt")
