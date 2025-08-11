cee5301f28dbd9c138c5f2344e20d1a2948ad36e
sysol8 <iabrosimov@list.ru>
2025-08-11 18:56:13 +0300

    Добавил провайдер для модалки в лейаут

--- src/layouts/AppLayout/AppLayout.tsx
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -2,10 +2,12 @@ import styles from './AppLayout.module.css';
 import { Outlet } from "react-router";
 import Header from "../../components/layout/Header/Header.tsx";
 import Footer from "../../components/layout/Footer/Footer.tsx";
+import Modal from "../../components/Modal/Modal.tsx";
 
 export default function AppLayout() {
   return (
     <div className={`${styles.layout}`}>
+      <Modal></Modal>
       <Header></Header>
       <Outlet />
       <Footer></Footer>
