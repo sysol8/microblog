5a767c5182ad0952befad6cd759213085ee3b0dc
sysol8 <iabrosimov@list.ru>
2025-08-12 19:31:04 +0300

    Заменил хардкод на константы из конфига

--- server/app/db/database.py
+++ server/app/db/database.py
@@ -1,10 +1,11 @@
 from sqlmodel import SQLModel, create_engine, Session
+from server.config import DATABASE_URL, SQL_ECHO, SQLITE_CHECK_SAME_THREAD
 
 DATABASE_URL = "sqlite:///./db.sqlite3"
 engine = create_engine(
     DATABASE_URL,
-    echo=True,
-    connect_args={"check_same_thread": False}
+    echo=SQL_ECHO,
+    connect_args={"check_same_thread": SQLITE_CHECK_SAME_THREAD} if DATABASE_URL.startswith("sqlite") else {}
 )
 
 def get_session():
