d4b325e03a4a9b98f9d71537b7b20fa258acdbbc
sysol8 <iabrosimov@list.ru>
2025-08-14 18:09:12 +0300

    Теперь User определяется раньше Post; Лайки теперь не число, а массив ID лайкнувших пользователей.

--- server/app/models.py
+++ server/app/models.py
@@ -1,18 +1,11 @@
-from __future__ import annotations
+# server/app/models.py
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 from sqlmodel import SQLModel, Field, Column, JSON, Relationship
+from uuid import uuid4
 
-class Post(SQLModel, table=True):
-    __tablename__ = "post"
-
-    id: str | None = Field(default=None, primary_key=True)
-    text_content: str
-    image_urls: List[str] = Field(default_factory=list, sa_column=Column(JSON, nullable=False))
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    created_by: str = Field(foreign_key="user.id", index=True)
-    author: User | None = Relationship(back_populates="posts")
-    likes: List[str] = Field(default_factory=list, sa_column=Column(JSON, nullable=False))
+def get_id() -> str:
+    return uuid4().hex
 
 class User(SQLModel, table=True):
     __tablename__ = "user"
@@ -26,8 +19,23 @@ class User(SQLModel, table=True):
 
     posts: list["Post"] = Relationship(
         back_populates="author",
-        sa_relationship_kwargs={"cascade": "all, delete-orphan"}
+        sa_relationship_kwargs={"cascade": "all, delete-orphan"},
     )
 
     liked: List[str] = Field(default_factory=list, sa_column=Column(JSON, nullable=False))
     likes: int = Field(default=0, ge=0)
+
+class Post(SQLModel, table=True):
+    __tablename__ = "post"
+
+    id: str | None = Field(default_factory=get_id, primary_key=True)
+    text_content: str
+    image_urls: List[str] = Field(default_factory=list, sa_column=Column(JSON, nullable=False))
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+
+    created_by: str | None = Field(default=None, foreign_key="user.id", index=True)
+
+    author: Optional["User"] = Relationship(back_populates="posts")
+
+    likes: List[str] = Field(default_factory=list, sa_column=Column(JSON, nullable=False))
+
