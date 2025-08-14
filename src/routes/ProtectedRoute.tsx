92f4aa2dda8eb85fe17c17720ccf929c102a9f4b
sysol8 <iabrosimov@list.ru>
2025-08-14 18:12:44 +0300

    Добавил защищенные роуты, которые редиректят незалогиненных пользователей на страничку логина

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { Navigate, useLocation, Outlet } from "react-router";
+import { useAuthStore } from "../store/authStore.ts";
+
+function ProtectedRoute() {
+  const { user } = useAuthStore();
+  const location = useLocation();
+
+  if (user === undefined) return null;
+
+  if (user === null) {
+    return <Navigate to="/login" replace state={{ from: location }} />
+  }
+
+  return <Outlet />
+}
+
+export default ProtectedRoute;
