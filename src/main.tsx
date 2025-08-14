4ea7d32df9f0c1f4b273a35bc81e6d39217b80a6
sysol8 <iabrosimov@list.ru>
2025-08-14 18:06:39 +0300

    Теперь main.tsx отвечает только за монтирование приложения, роутинг перенесен

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,7 @@
 // @ts-expect-error / подключение шрифта fontsource
 import "@fontsource-variable/open-sans";
 import './index.css'
-
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router";
+import App from "./App.tsx";
 
-import AppLayout from './layouts/AppLayout/AppLayout.tsx'
-import PostsLayout from "./layouts/PostsLayout/PostsLayout.tsx";
-import AuthLayout from "./layouts/AuthLayout/AuthLayout.tsx";
-
-
-createRoot(document.querySelector('#root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<PostsLayout />} />
-          <Route path="users/me" element={<AuthLayout />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+createRoot(document.querySelector('#root')!).render(<App />)
