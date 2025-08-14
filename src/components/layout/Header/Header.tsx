19694c59a355749a764f9a059c5cfeae56454e3b
sysol8 <iabrosimov@list.ru>
2025-08-14 18:05:00 +0300

    Теперь нет роута "Мои посты", будет перенесено в профиль

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,25 +1,28 @@
-import styles from './Header.module.css';
-import { Link } from 'react-router';
+import styles from "./Header.module.css";
+import { Link } from "react-router";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.layout}>
-        <Link to="/" className={styles.logoLink}>Placeholder</Link>
+        <Link to="/" className={styles.logoLink}>
+          Placeholder
+        </Link>
         <nav className={styles.nav}>
           <ul className={styles.list}>
             <li className={styles.item}>
-              <Link to="/" className={styles.link}>Лента</Link>
+              <Link to="/" className={styles.link}>
+                Лента
+              </Link>
             </li>
             <li className={styles.item}>
-              <Link to="/posts/my" className={styles.link}>Мои посты</Link>
-            </li>
-            <li className={styles.item}>
-              <Link to="/users/me" className={styles.link}>Профиль</Link>
+              <Link to="/users/me" className={styles.link}>
+                Профиль
+              </Link>
             </li>
           </ul>
         </nav>
       </div>
     </header>
-  )
+  );
 }
