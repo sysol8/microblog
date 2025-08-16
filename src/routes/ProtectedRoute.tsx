bf20453f46ed0f4d2f028325728e8eb73d1d90c8
sysol8 <iabrosimov@list.ru>
2025-08-16 19:24:39 +0300

    Добавил логирование для отладки

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -4,14 +4,15 @@ import { useAuthStore } from "../store/authStore.ts";
 function ProtectedRoute() {
   const { user } = useAuthStore();
   const location = useLocation();
+  console.log(user);
 
   if (user === undefined) return null;
 
   if (user === null) {
-    return <Navigate to="/login" replace state={{ from: location }} />
+    return <Navigate to="/login" replace state={{ from: location }} />;
   }
 
-  return <Outlet />
+  return <Outlet />;
 }
 
 export default ProtectedRoute;
