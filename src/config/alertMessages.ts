db8bdc08002edb396278842ed9b42c27cd0c856f
sysol8 <iabrosimov@list.ru>
2025-08-16 19:12:33 +0300

    Добавил конфиг для хранения сообщений для компонента Alert

---
+++ src/config/alertMessages.ts
@@ -0,0 +1,12 @@
+export const alertMessages = {
+  post: {
+    addSuccess: "Пост успешно опубликован",
+    addError: "Не удалось опубликовать пост",
+  },
+  auth: {
+    authRequired: "Войдите или зарегистрируйтесь, чтобы выполнить действие",
+  },
+  common: {
+    network: "Ошибка сети. Повторите попытку позже"
+  }
+} as const;
