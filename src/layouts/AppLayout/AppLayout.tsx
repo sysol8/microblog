6737cb6a32bbfffad86c9b5111f399a75ea2d891
sysol8 <iabrosimov@list.ru>
2025-08-16 19:16:32 +0300

    Добавил тостер в лейаут приложения, пока тестирую.

--- src/layouts/AppLayout/AppLayout.tsx
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -3,11 +3,13 @@ import { Outlet } from "react-router";
 import Header from "../../components/layout/Header/Header.tsx";
 import Footer from "../../components/layout/Footer/Footer.tsx";
 import Modal from "../../components/Modal/Modal.tsx";
+import AlertToaster from "../../components/Toaster/AlertToaster/AlertToaster.tsx";
 
 export default function AppLayout() {
   return (
     <div className={`${styles.layout}`}>
       <Modal></Modal>
+      <AlertToaster></AlertToaster>
       <Header></Header>
       <Outlet />
       <Footer></Footer>
