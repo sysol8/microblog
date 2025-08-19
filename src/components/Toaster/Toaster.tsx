09ed6b73428f7a3e5591dccc91fb2c477832b0f7
sysol8 <iabrosimov@list.ru>
2025-08-19 17:03:55 +0300

    Теперь при добавлении нового итема контейнер скроллится вниз, куда и добавился этот итем

--- src/components/Toaster/Toaster.tsx
+++ src/components/Toaster/Toaster.tsx
@@ -1,5 +1,6 @@
 import styles from "./Toaster.module.css";
 import { createPortal } from "react-dom";
+import { useRef, useEffect } from "react";
 import type { ReactNode } from "react";
 
 export interface ToasterItem {
@@ -13,14 +14,22 @@ export interface ToastProps<T extends ToasterItem> {
 
 function Toaster<T extends ToasterItem>({ items, renderItem }: ToastProps<T>) {
   const toasterContainer = document.getElementById("toaster-root");
+  const listRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    const list = listRef.current;
+    if (list) {
+      list.scrollTop = list.scrollHeight;
+    }
+  }, [items.length])
 
   if (!toasterContainer) return null;
 
   return createPortal(
     <div className={styles.toaster}>
-      <ul className={styles.list}>
+      <ul className={styles.list} ref={listRef}>
         {items.map((item) => (
-          <li key={item.id}>{renderItem(item)}</li>
+          <li key={item.id} className={styles.item}>{renderItem(item)}</li>
         ))}
       </ul>
     </div>,
