4c76befca03786fd9b59a74520106eeb578fd6b0
sysol8 <iabrosimov@list.ru>
2025-08-21 18:12:53 +0300

    Теперь ensureOk выкидывает человекочитаемые ошибки при определенных статусах ответа

--- src/api/base.ts
+++ src/api/base.ts
@@ -10,6 +10,12 @@ async function parseJson<T>(response: Response): Promise<T> {
 
 async function ensureOk(response: Response): Promise<Response> {
   if (!response.ok) {
+    // HACK: Простая обработка с выбрасыванием человекочитаемых ошибок.
+    //  В данном случае такой обработки достаточно, но при расширении проекта можно вынести
+    //  обработку в отдельную сущность и сделать более универсальной
+    if (response.status === 400) throw new Error("Введены некорректные данные");
+    if (response.status === 401) throw new Error("Неверный логин или пароль");
+    if (response.status === 409) throw new Error("Пользователь с таким именем уже существует");
     throw new Error(`Ошибка ${response.status}: ${response.statusText}`)
   }
   return response;
