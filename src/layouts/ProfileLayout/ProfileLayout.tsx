e6e4c05d50d26db53716a49ac0810d6a32bc147c
sysol8 <iabrosimov@list.ru>
2025-08-22 18:30:45 +0300

    Теперь лейаут использует компонент Avatar. Обновлена логика редиректов и получения данных.

--- src/layouts/ProfileLayout/ProfileLayout.tsx
+++ src/layouts/ProfileLayout/ProfileLayout.tsx
@@ -1,123 +1,108 @@
 import styles from "./ProfileLayout.module.css";
 import LikeIcon from "../../assets/icons/like.svg?react";
 import type { IUser } from "../../utils/types.ts";
-import { useEffect, useState, useMemo } from 'react';
+import { useCallback, useEffect, useState } from "react";
 import LogoutButton from "../../components/auth/Logout/LogoutButton.tsx";
 import PostCompact from "../../components/Post/PostCompact/PostCompact.tsx";
 import { getUser } from "../../api/users.ts";
-import { useParams } from "react-router";
+import { useAuthStore } from "../../store/authStore.ts";
+import { useParams, useNavigate } from "react-router";
+import Loader from "../../components/Loader/Loader.tsx";
+import Avatar from "../../components/User/Avatar/Avatar.tsx";
 
 interface ProfileLayoutProps {
-  data: IUser;
   isMe: boolean;
 }
 
-function ProfileLayout({ data, isMe }: ProfileLayoutProps) {
+function ProfileLayout({ isMe }: ProfileLayoutProps) {
+  const [data, setData] = useState<IUser | null>(null);
+  const user = useAuthStore((state) => state.user);
   const { username } = useParams();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
 
-  // TEMP: временное решение (и временно не работающее). По текущей задумке
-  //  компонент фетчит данные пользователя, если флаг isMe установлен на false.
-  //  В противном случае берутся данные авторизованного пользователя из глобального стейта.
-  /*
-  const [remote, setRemote] = useState<IUser | null>(null);
-  const [loading, setLoading] = useState<boolean>(!isMe);
-  const [error, setError] = useState<string | null>(null);
-
+  const redirectToUser = useCallback(async () => {
+    if (isMe && user) {
+      setData(user);
+    } else if (!isMe && username) {
+      if (username === user?.username) {
+        navigate("/users/me", { replace: true });
+      } else {
+        try {
+          setLoading(true);
+          const response = await getUser(username);
+          setData(response);
+          setLoading(false);
+        } catch {
+          console.error();
+        }
+      }
+    }
+  }, [user, username, isMe, navigate]);
 
   useEffect(() => {
-    if (isMe) return;
+    redirectToUser();
+  }, [redirectToUser]);
 
-    const ac = new AbortController();
-    (async () => {
-      try {
-        setLoading(true);
-        setError(null);
-        if (!username) {
-          setError("Пользователь не указан");
-          return;
-        }
-        const res = await getUser(username);
-        setRemote(res);
-      } catch (e) {
-        if ((e instanceof Error).name !== "AbortError") {
-          setError(e instanceof Error ? e.message : "Ошибка загрузки профиля");
-        }
-      } finally {
-        setLoading(false);
-      }
-    })();
+  if (loading && !data) return <Loader />;
 
-    return () => ac.abort();
-  }, [isMe, username]);
-
-  const profile = useMemo<IUser | null>(() => {
-    return isMe ? (data ?? null) : remote;
-  }, [isMe, data, remote]);
-
-  if (!isMe && loading) return <p>Загрузка…</p>;
-  if (!profile) return <p>{error ?? "Пользователь не найден"}</p>;
-  */
-
-  return (
-    <main className={styles.content}>
-      <div className={styles.wrapper}>
-        <div className={styles.header}>
-        <div className={styles.user}>
-          <img
-            className={styles.avatar}
-            src={data.avatarUrl ? data.avatarUrl : "/avatar-placeholder.png"}
-            alt={`Аватар пользователя ${data.username}`}
-          />
-          <h2 className={styles.username}>{data.username}</h2>
-        </div>
-        {isMe ? <LogoutButton /> : null}
-      </div>
-        <section className={styles.data}>
-          <article className={styles.article}>
-            <h3 className={styles.articleHeading}>
-              {isMe ? "Мои посты" : "Посты"}
-            </h3>
-            {data.posts.length > 0 ? (
-              <ul className={styles.posts}>
-                {data.posts.map((post, index) => (
-                  <li key={post.id} className={styles.post}>
-                    <span className={styles.counter}>{index + 1}</span>
-                    <PostCompact data={post}></PostCompact>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>Здесь пока пусто...</p>
-            )}
-          </article>
-          <article className={styles.article}>
-            <h3 className={styles.articleHeading}>Понравившиеся</h3>
-            {data.likedPosts.length > 0 ? (
-              <ul className={styles.posts}>
-                {data.likedPosts.map((liked, index) => (
-                  <li key={liked.id} className={styles.post}>
-                    <span className={styles.counter}>{index + 1}</span>
-                    <PostCompact data={liked}></PostCompact>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>Здесь пока пусто...</p>
-            )}
-          </article>
-          <article className={`${styles.article}`}>
-            <h3 className={styles.articleHeading}>
-              {isMe ? "Мои лайки" : "Лайки"}
-            </h3>
-            <div className={styles.likesArticle}>
-              <LikeIcon className={styles.likeIcon} />
-              <span className={styles.likes}>{data.likes}</span>
+  if (data)
+    return (
+      <main className={styles.content}>
+        <div className={styles.wrapper}>
+          <div className={styles.header}>
+            <div className={styles.user}>
+              <Avatar mode={"profile"} username={data.username} src={data.avatarUrl} isMe={isMe} />
+              <h2 className={styles.username}>{data.username}</h2>
             </div>
-          </article>
-        </section>
-      </div>
-    </main>
-  );
+            {isMe ? <LogoutButton /> : null}
+          </div>
+          <section className={styles.data}>
+            <article className={styles.article}>
+              <h3 className={styles.articleHeading}>
+                {isMe ? "Мои посты" : "Посты"}
+              </h3>
+              {data.posts.length > 0 ? (
+                <ul className={styles.posts}>
+                  {data.posts.map((post, index) => (
+                    <li key={post.id} className={styles.post}>
+                      <span className={styles.counter}>{index + 1}</span>
+                      <PostCompact data={post}></PostCompact>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>Здесь пока пусто...</p>
+              )}
+            </article>
+            <article className={styles.article}>
+              <h3 className={styles.articleHeading}>Понравившиеся</h3>
+              {data.likedPosts.length > 0 ? (
+                <ul className={styles.posts}>
+                  {data.likedPosts.map((liked, index) => (
+                    <li key={liked.id} className={styles.post}>
+                      <span className={styles.counter}>{index + 1}</span>
+                      <PostCompact data={liked}></PostCompact>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>Здесь пока пусто...</p>
+              )}
+            </article>
+            <article className={`${styles.article}`}>
+              <h3 className={styles.articleHeading}>
+                {isMe ? "Мои лайки" : "Лайки"}
+              </h3>
+              <div className={styles.likesArticle}>
+                <LikeIcon className={styles.likeIcon} />
+                <span className={styles.likes}>{data.likes}</span>
+              </div>
+            </article>
+          </section>
+        </div>
+      </main>
+    );
 }
 
 export default ProfileLayout;
