71ccfb235785d4a45adbfddf83fc1fd147604099
sysol8 <iabrosimov@list.ru>
2025-08-31 17:53:27 +0300

    chore: обновил конфиг для использования с докером

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,9 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [svgr(), react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: { usePolling: true, interval: 200 }
+  }
 })
