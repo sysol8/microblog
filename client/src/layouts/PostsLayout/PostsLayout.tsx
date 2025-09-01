6d3c3bff9c8b0f8bd4707c346ec3d3970281f7b4
sysol8 <iabrosimov@list.ru>
2025-09-01 17:02:03 +0300

    refactor: clsx вместо конкатенации

--- client/src/layouts/PostsLayout/PostsLayout.tsx
+++ client/src/layouts/PostsLayout/PostsLayout.tsx
@@ -2,6 +2,7 @@ import styles from "./PostsLayout.module.css";
 import PostsList from "../../components/PostsList/PostsList.tsx";
 import PostForm from "../../components/PostForm/PostForm.tsx";
 import usePosts from "../../hooks/usePosts.ts";
+import clsx from 'clsx'
 
 export default function PostsLayout() {
   const { posts, deletePost, addPost } = usePosts();
@@ -12,7 +13,7 @@ export default function PostsLayout() {
         <PostsList posts={posts} onDelete={deletePost}></PostsList>
         <PostForm onPostAdd={addPost}></PostForm>
       </main>
-      <aside className={`${styles.aside} ${styles.toasterRoot}`} id="toaster-root"></aside>
+      <aside className={clsx(styles.aside, styles.toasterRoot)} id="toaster-root"></aside>
     </div>
   );
 }
