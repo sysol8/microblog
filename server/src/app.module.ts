dc0fde05ffbdeaa419c656a6802f7a9c4337014b
sysol8 <iabrosimov@list.ru>
2025-09-08 17:03:23 +0300

    feat(validation): добавлен сгенерированный при инициализации zod код

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,62 @@
-import { Module } from '@nestjs/common';
+import {
+  ZodValidationPipe,
+  ZodSerializerInterceptor,
+  ZodSerializationException,
+} from 'nestjs-zod';
+import {
+  APP_PIPE,
+  APP_INTERCEPTOR,
+  APP_FILTER,
+  BaseExceptionFilter,
+} from '@nestjs/core';
+import { ZodError } from 'zod';
+import {
+  Module,
+  HttpException,
+  ArgumentsHost,
+  Logger,
+  Catch,
+} from '@nestjs/common';
+import { configModule } from '../config/config.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsController } from './posts/posts.controller';
+import { UsersController } from './users/users.controller';
+
+@Catch(HttpException)
+class HttpExceptionFilter extends BaseExceptionFilter {
+  private logger = new Logger(HttpExceptionFilter.name);
+
+  catch(exception: HttpException, host: ArgumentsHost) {
+    if (exception instanceof ZodSerializationException) {
+      const zodError = exception.getZodError();
+
+      if (zodError instanceof ZodError) {
+        this.logger.error(`ZodSerializationException: ${zodError.message}`);
+      }
+    }
+
+    super.catch(exception, host);
+  }
+}
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [configModule],
+  controllers: [AppController, UsersController, PostsController],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ZodSerializerInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
