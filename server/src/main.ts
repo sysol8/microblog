30f43b4d2ff520680826d7a92e6abb583293157f
sysol8 <iabrosimov@list.ru>
2025-09-08 17:44:53 +0300

    feat(swagger): добавлена Swagger-документация API

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,31 @@
+import { cleanupOpenApiDoc } from 'nestjs-zod';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const openApiDoc = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Example API')
+      .setDescription('Example API description')
+      .setVersion('1.0')
+      .build(),
+  );
+
+  SwaggerModule.setup('api', app, cleanupOpenApiDoc(openApiDoc));
+
+  const config = new DocumentBuilder()
+    .setTitle('Microblog API')
+    .setDescription('Microblog API')
+    .setVersion('1.0')
+    .addTag('microblog')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
