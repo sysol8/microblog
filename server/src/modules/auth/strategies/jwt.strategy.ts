abd0aec70147c68cb2eb713a65d893dbaba81537
sysol8 <iabrosimov@list.ru>
2025-10-03 18:29:53 +0300

    feat(auth): добавил стратегию аутентификации пользователя по JWT

---
+++ server/src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtDto } from '../dto/jwt.dto';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow('JWT_ACCESS_SECRET'),
+    });
+  }
+
+  validate(payload: JwtDto) {
+    return { id: payload.id, username: payload.username };
+  }
+}
